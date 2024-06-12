4e9c6cd7b7358f204de75edbac381e0801a1c5b1
nahyer <reyhanluyai@gmail.com>
2024-06-12 10:22:59 +0300

    db

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -3,8 +3,8 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-// export const client = neon(process.env.Database_URL as string)
-export const sql = neon("postgresql://RestorantDB_owner:iQB4akYtu8cF@ep-rough-pine-a509smkk.us-east-2.aws.neon.tech/RestorantDB?sslmode=require")
+export const sql = neon(process.env.Database_URL as string)
+// export const sql = neon("postgresql://RestorantDB_owner:iQB4akYtu8cF@ep-rough-pine-a509smkk.us-east-2.aws.neon.tech/RestorantDB?sslmode=require")
 const db = drizzle(sql, { schema, logger: true }) 
 
 export default db; 
